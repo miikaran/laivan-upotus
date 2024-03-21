354ad5f41713ddb838abc05adaa2f6c7ea940d53
miikaran <42628@edu.raseko.fi>
2024-03-21 20:18:15 +0200

    lisäsin menun, pelaajien luonnin, sekä paransin taulun tulostusta

--- laivanupotus.java
+++ laivanupotus.java
@@ -1,71 +1,173 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class laivanupotus {
 
     /**
-     * Luodaan pelin kannalta kriittiset kiinteät muuttujat.
+     * Luodaan pelin kannalta 
+     * kriittiset muuttujat.
      */
     static final Scanner scanner = new Scanner(System.in);
     static String sarakeKirjaimet = "ABCDEFGHIJ";
     static int rivit = 10;
     static int sarakkeet = 10;
-    static String [][] taulu = new String[rivit][sarakkeet];
     static String[] laivat = {"Sukellusvene", "Hävittäjä", "Risteilijä", "Taistelulaiva", "Lentotukialus"};
     static int[] laivaKoot = {1,2,3,4,5};
-
+    static String pelimuoto = "";
+    static boolean peliPaalla = false;
+    static String[] merkit = {"~", "O", "H", "M"};
+    static String[] pelaajat = new String[2];
+    static HashMap<String, String[][]> pelaajienTaulut = new HashMap<String, String[][]>();
+    static String vuoro = "" ;
 
     /**
-     * Tästä metodista aloitetaan peli.
+     * Aloitetaan peli.
      */
     public static void main(String[] args){
-        taulujenLuonti();
-        tulostaTaulu();
-        kysyJaValidoiLaivat();
+        peliPaalla = true;
+        menu();
+        luoPelaajat(pelimuoto);
+        luoTaulut();
+        kysyLaivat();
     }
 
     /**
-     * Palauttaa pelaajien taulukot.
+     * Näytetään menu ja kysytään käyttäjältä pelimuoto.
      */
-    public static String taulujenLuonti(){
+    public static void  menu(){
+        System.out.println("=====================================");
+        System.out.println("\n Tervetuloa pelaamaan laivanupotusta! ");
+        System.out.println("\n Valitse pelimuoto jota haluat pelata:");
+        System.out.println(" 1 => Tietokonetta vastaan\n 2 => Kaveria vastaan\n 3 => Lopeta peli");
+        System.out.println("\n=====================================");
+        System.out.print("\n=> ");
+        int kayttajanSyote = scanner.nextInt();
+        switch (kayttajanSyote){
+            case 1:
+              pelimuoto = "tietokone";
+              break;
+            case 2:
+              pelimuoto = "kaveri";
+              break;
+            case 3:
+              System.out.println("Suljetaan ohjelma...");
+              System.exit(0);
+        }
+    }
+
+    public static void luoPelaajat(String pelimuoto){
+        if(pelimuoto.equals("kaveri")){
+            scanner.nextLine();
+            for(int i = 0; i < 2; i++){
+                while(true){
+                    System.out.print("\nPelaaja " + (i+1) + "\nAseta nimesi: \n=>");
+                    String syote = scanner.nextLine();
+                    if(syote.length() < 1){
+                        System.out.println("Yritä uudelleen...");
+                        continue;
+                    }
+                    pelaajat[i] = syote;
+                    break;
+                }
+            }
+        } else{
+            pelaajat[0] = "Pelaaja";
+            pelaajat[1] = "Tietokone";
+        }
+    }
+
+    /**
+     * Luodaan pelaajien taulut.
+     */
+    public static void luoTaulut(){
+        String[][] taulu = new String[rivit][sarakkeet];
         for(int i = 0; i < rivit; i++){
             for(int j = 0; j < sarakkeet; j++){ 
-                taulu[i][j] = "~"; 
+                taulu[i][j] = merkit[0]; 
             }
-        } return Arrays.deepToString(taulu);
+        }
+        for(String pelaaja : pelaajat){
+            pelaajienTaulut.put(pelaaja, taulu);
+        }
+    }
+
+    public static void aloitaTaistelu(){
+        vuoro = pelaajat[0];
+        while(peliPaalla){
+            System.out.println(vuoro + ":n" + " vuoro" );
+            tulostaTaulu(vuoro);
+        }
     }
 
     /**
-     * Tulostaa pelaajan taulun.
+     * Tulostaa pelaajan taulun koko taulun.
      * Lisää tulokseen myös osoittajia taulun riveistä ja sarakkeista.
      */
-    public static void tulostaTaulu(){
-        System.out.print("  ");
-        for(char kirjain : sarakeKirjaimet.toCharArray()){
-            System.out.print(kirjain + " ");
+    public static void tulostaTaulu(String pelaaja) {
+        String[][] taulu = pelaajienTaulut.get(pelaaja);
+        int korkeus = taulu.length;
+        int leveys = taulu[0].length;
+    
+        System.out.print("   ");
+        for (int j = 0; j < leveys; j++) {
+            System.out.print(" " + (char)('A' + j) + "  ");
         }
         System.out.println();
-
-        for(int i = 0; i < taulu.length; i++){
-            System.out.print((i) + " ");
-            for(String alkio : taulu[i]){
-                System.out.print(alkio + " ");
+    
+        System.out.print("  ┌");
+        for (int i = 0; i < leveys; i++) {
+            System.out.print("───");
+            if (i < leveys - 1) {
+                System.out.print("┬");
             }
-            System.out.println();
         }
+        System.out.println("┐");
+    
+        for (int i = 0; i < korkeus; i++) {
+            System.out.print(" " + (i) + "│");
+            for (int j = 0; j < leveys; j++) {
+                System.out.print(" " + taulu[i][j] + " ");
+                if (j < leveys - 1) {
+                    System.out.print("│");
+                }
+            }
+            System.out.println("│");
+    
+            if (i < korkeus - 1) {
+                System.out.print("  ├");
+                for (int j = 0; j < leveys; j++) {
+                    System.out.print("───");
+                    if (j < leveys - 1) {
+                        System.out.print("┼");
+                    }
+                }
+                System.out.println("┤");
+            }
+        }
+    
+        System.out.print("  └");
+        for (int i = 0; i < leveys; i++) {
+            System.out.print("───");
+            if (i < leveys - 1) {
+                System.out.print("┴");
+            }
+        }
+        System.out.println("┘");
     }
-
+    
+    
     /**
      * Asettaa uuden laivan pelaajan taulukolle sille annettujen
-     * aloitus ja lopetus parametrien mukaan ja tulostaa sen lopuksi.
+     * aloitus ja lopetus parametrien mukaan.
      */
-    public static void asetaLaiva(int aloitusRivi, int aloitusSarake, int lopetusRivi, int lopetusSarake){
+    public static void asetaLaiva(int aloitusRivi, int aloitusSarake, int lopetusRivi, int lopetusSarake, String pelaaja){
         for(int i = aloitusRivi; i <= lopetusRivi; i++){
             for(int j = aloitusSarake; j <= lopetusSarake; j++){
-                taulu[i][j] = "X";
+                String[][] taulu = pelaajienTaulut.get(pelaaja);
+                taulu[i][j] = merkit[1];
             }
         }
-        tulostaTaulu();
     }
 
     /**
@@ -73,65 +175,91 @@ public class laivanupotus {
      * syötteet taulukossa valmiiksi olevien laivojen mukaan sekä 
      * tarkistaa, että laivat lisätään taulukkoon sääntöjen mukaisesti.
      */
-    public static void kysyJaValidoiLaivat(){
+    public static void kysyLaivat(){
         int laivaMaara = laivat.length;
-        for(int i = 0; i < laivaMaara; i++){
-            String laiva = laivat[i];
-            int koko = laivaKoot[i];
 
-            while (true) {
-                System.out.println("\nLaiva: " + laiva + " -> Koko: " + koko);
-                System.out.print("Aseta oman laivan koordinaatit\n=> ");
+        for(String pelaaja : pelaajat){
+            String[][] taulu = pelaajienTaulut.get(pelaaja);
+            for(int i = 0; i < laivaMaara; i++){
+                String laiva = laivat[i];
+                int koko = laivaKoot[i];
+                tulostaTaulu(pelaaja);
+        
+                while (true) {
+                    System.out.println("\n" + pelaaja);
+                    System.out.print("\nAseta oman laivan koordinaatit");
+                    System.out.print("\nLaiva: " + laiva + " -> Koko: " + koko + "\n=> ");
 
-                // Otetaan käyttäjän koordinaatit ja erotellaan ne.
-                String[] koordinaatit = scanner.nextLine().split(" ");
-                
-                // Rivi ja sarake, joista laivaa lähdetään rakentamaan.
-                int aloitusRivi = Integer.parseInt(koordinaatit[0].substring(1));
-                int aloitusSarake = sarakeKirjaimet.indexOf(koordinaatit[0].charAt(0));
-                
-                // Rivi ja sarake, johon laivan rakennus lopetetaan.
-                int lopetusRivi = Integer.parseInt(koordinaatit[1].substring(1));
-                int lopetusSarake = sarakeKirjaimet.indexOf(koordinaatit[1].charAt(0));
+                    // Otetaan käyttäjän koordinaatit ja erotellaan ne.
+                    String[] koordinaatit = new String[2];
+                    try{ // Otetaan koordinaatit muodossa => kirjain+sarake
+                        koordinaatit = scanner.nextLine().toUpperCase().split(" ");
+                    } catch(Exception NumberFormatException){
+                        System.out.println("Aseta koordinaatit muodossa: sarake+rivi");
+                    }
 
-                boolean laivaaVoiSijoittaa = true;
+                    Arrays.sort(koordinaatit);
 
-                //Tarkistetaan onko koordinaattien lähellä jo laiva. 
-                for(int j = aloitusRivi; j <= lopetusRivi; j++){
-                    for(int k = aloitusSarake; k <= lopetusSarake; k++){
-                        if(!taulu[j][k].equals("~")   ||  // Päällä
-                           !taulu[j][k-1].equals("~") ||  // Vasemmalla
-                           !taulu[j][k+1].equals("~") ||  // Oikealla
-                           !taulu[j-1][k].equals("~") ||  // Ylhäällä
-                           !taulu[j+1][k].equals("~")){   // Alhaalla
-                            laivaaVoiSijoittaa = false;
+                    // Rivi ja sarake, joista laivaa lähdetään rakentamaan.
+                    int aloitusRivi = Integer.parseInt(koordinaatit[0].substring(1));
+                    int aloitusSarake = sarakeKirjaimet.indexOf(koordinaatit[0].charAt(0));
+
+                    
+                    // Rivi ja sarake, johon laivan rakennus lopetetaan.
+                    int lopetusRivi = Integer.parseInt(koordinaatit[1].substring(1));
+                    int lopetusSarake = sarakeKirjaimet.indexOf(koordinaatit[1].charAt(0));
+
+
+                    boolean laivaaVoiSijoittaa = true;
+
+
+                    //Tarkistetaan onko koordinaattien lähellä jo laivaa. 
+                    for(int j = aloitusRivi; j <= lopetusRivi; j++){
+
+                        for(int k = aloitusSarake; k <= lopetusSarake; k++){
+
+                            try{
+                                
+                                if(taulu[j][k].equals("O")||  // Päällä
+                                taulu[j][k-1].equals("O") ||  // Vasemmalla
+                                taulu[j][k+1].equals("O") ||  // Oikealla
+                                taulu[j-1][k].equals("O") ||  // Ylhäällä
+                                taulu[j+1][k].equals("O")     // Alhaalla
+                                )
+                                { 
+                                    laivaaVoiSijoittaa = false;
+                                }
+                                
+                            } catch (Exception IndexOutOfBoundsException) {
+                                // Jos tarkistukset menee yli rajojen, jatketaan peliä.
+                                System.out.println();
+
+                            }
                         }
                     }
-                }
-            
-                /**
-                 *  Alla muita olennaisia laivojen asettamiseen liityviä validointeja.
-                    * Koordinaattien määrä
-                    * Viistoossa olevat laivat
-                    * Laivan koko.
-                */
-                if (koordinaatit.length != 2) {
-                    System.out.println("Syötä 2 koordinaatiia!");
-                } else if (aloitusRivi != lopetusRivi && aloitusSarake != lopetusSarake) { // 
-                    System.out.println("Et voi asettaa laivoja viistoon.");
-                } else if (lopetusRivi - aloitusRivi + 1 > koko || lopetusSarake - aloitusSarake + 1 > koko) {
-                    System.out.println("Laivan koko on " + koko + ". Syötä koordinaatit uudelleen.");
-                } else if(!laivaaVoiSijoittaa){
-                    System.out.println("Et voi laittaa tähän kohtaan laivaa.");
-                }
-                else{ /* Mikäli kaikki suoritetut validaatiot menee läpi, 
-                         asetetaan laiva haluttuun positioon. */
-                    asetaLaiva(aloitusRivi, aloitusSarake, lopetusRivi, lopetusSarake);
-                    break;
+                
+                    /**
+                     *  Alla muita olennaisia laivojen asettamiseen liityviä validointeja.
+                        * Koordinaattien määrä
+                        * Viistoossa olevat laivat
+                        * Laivan koko.
+                    */
+                    if (koordinaatit.length != 2) {
+                        System.out.println("\nSyötä 2 koordinaattia!");
+                    } else if (aloitusRivi != lopetusRivi && aloitusSarake != lopetusSarake) { // 
+                        System.out.println("\nEt voi asettaa laivoja viistoon.");
+                    } else if (lopetusRivi - aloitusRivi + 1 > koko || lopetusSarake - aloitusSarake + 1 > koko) {
+                        System.out.println("\nLaivan koko on " + koko + ". Syötä koordinaatit uudelleen.");
+                    } else if(!laivaaVoiSijoittaa){
+                        System.out.println("\nEt voi laittaa tähän kohtaan laivaa.");
+                    }
+                    else{ /* Mikäli kaikki suoritetut validaatiot menee läpi, 
+                            asetetaan laiva haluttuun positioon. */
+                        asetaLaiva(aloitusRivi, aloitusSarake, lopetusRivi, lopetusSarake, pelaaja);
+                        break;
+                    }
                 }
             }
-            
         }
-        scanner.close();
     }
 }
