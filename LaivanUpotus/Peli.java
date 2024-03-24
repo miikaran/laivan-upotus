3c331543364a64b4124d198c6b9546f768cf2933
miikaran <42628@edu.raseko.fi>
2024-03-24 09:47:40 +0200

    pelaajan hyokkays metodia pienempiin osiin.

--- LaivanUpotus/Peli.java
+++ LaivanUpotus/Peli.java
@@ -17,6 +17,7 @@ public class Peli {
     public static HashMap<String, String[][]> pelaajienKartat = new HashMap<String, String[][]>();
     public static HashMap<String, int[][]> pelaajienLaivat = new HashMap<String, int[][]>();
     public static int vuoro = 0;
+    public static String voittaja;
 
     /**
      * Suoritetaan tarvittavat alkutoimenpiteet ja aloitetaan peli.
@@ -56,69 +57,96 @@ public class Peli {
      * ilmoitetaan pelaajalle osuiko/upposiko.
      */
     public static void pelaajaHyokkaa(String vastustaja){
-        try{
-            while(true){
-                    // Otetaan käyttäjälta koordinaatti.
-                    String koordinaatti = scanner.next();
+        while(true){
+            try{
+                // Otetaan käyttäjälta koordinaatti.
+                String koordinaatti = scanner.next();
 
-                    // Muutetaan koordinaatit kartan riveiksi ja sarakkeiksi.
-                    int rivi = Integer.parseInt(koordinaatti.substring(1));
-                    int sarake = Vakiot.sarakeKirjaimet.indexOf(koordinaatti.charAt(0));
-                    
-                    // Haetaan vastustajan kartta ja laivat.
-                    String[][] vastustajanKartta = pelaajienKartat.get(vastustaja);
-                    int[][] vastaustajanLaivat = pelaajienLaivat.get(vastustaja);
+                // Muutetaan koordinaatit kartan riveiksi ja sarakkeiksi.
+                int rivi = Integer.parseInt(koordinaatti.substring(1));
+                int sarake = Vakiot.sarakeKirjaimet.indexOf(koordinaatti.charAt(0));
+                
+                // Haetaan vastustajan kartta ja laivat.
+                String[][] vastustajanKartta = pelaajienKartat.get(vastustaja);
+                int[][] vastustajanLaivat = pelaajienLaivat.get(vastustaja);
 
-                    //Tarkistetaan osusiko arvaus.
-                    if(vastustajanKartta[rivi][sarake].equals(Vakiot.merkit[1])){   
+                // Haetaan osuttu laiva, mikäli osui.
+                int[] osuttuLaiva = tarkistaOsuma(vastustajanKartta, vastustajanLaivat, rivi, sarake);
 
-                        // Jos osui merkitään se vastustajan karttaan.
-                        System.out.println("\nRäjähdyssss! Osuit Laivaan!");
-                        vastustajanKartta[rivi][sarake] = Vakiot.merkit[2];
-                        int[] osuttuLaiva = new int[4];
-        
-                        outerLoop: // Haetaan osuttu laiva vastustajan kartasta.
-                        for(int[] laiva : vastaustajanLaivat){
-                            // Oikea laiva jos laivan koordinaatit sama pelaajan arvaamien koordinattien kanssa.
-                            for (int i = laiva[0]; i <= laiva[1]; i++) {
-                                for (int j = laiva[2]; j <= laiva[3]; j++) {
-                                    if(i == rivi && j == sarake){
-                                        osuttuLaiva = laiva;
-                                        break outerLoop;
-                                    }
-                                }
-                            }
-                        }
-                        /*System.out.println(Arrays.deepToString(vastaustajanLaivat));
-                        System.out.println(rivi);
-                        System.out.println(sarake);
-                        System.out.println(Arrays.toString(osuttuLaiva));*/
-                        boolean upposko = false;
-                        outerLoop: // Tarkistetaan upposiko laiva arvauksella.
-                        for(int i = osuttuLaiva[0]; i <= osuttuLaiva[1]; i++){
-                            for (int j = osuttuLaiva[2]; j <= osuttuLaiva[3]; j++){
-                                // Jos osutun laivan jokaisessa osassa on osuma: laiva uppoaa.
-                                if(vastustajanKartta[i][j].equals(Vakiot.merkit[2])){
-                                    upposko = true;
-                                } else{ // Jos mitä tahansa muuta: ei uppoa.
-                                    upposko = false;
-                                    break outerLoop;
-                                }
-                            }
-                        }
-                        //MeriKartta.tulostaKartta(vastustaja);
-                        if(upposko){
-                            System.out.println("Uppos!!!!!!!!!! BOOOOOOOOOOOM.");
+                /*System.out.println(Arrays.deepToString(vastaustajanLaivat));
+                System.out.println(rivi);
+                System.out.println(sarake);
+                System.out.println(Arrays.toString(osuttuLaiva));
+                MeriKartta.tulostaKartta(vastustaja);*/
+
+                //Tarkistetaan osusiko arvaus.
+                if(osuttuLaiva.length > 0){
+
+                    // Tarkistetaan upposiko osumalla.
+                    boolean upposko = upposko(osuttuLaiva, vastustajanKartta);
+                    if(upposko){
+                        System.out.println("Uppos!!!!!!!!!! BOOOOOOOOOOOM.");
+                    }
+
+                } else {
+                    System.out.println("\nOhi");
+                }
+                
+                break;
+
+            } catch (Exception e){
+                System.out.println("Yritä uudelleen...");
+            } 
+        }  
+    }
+
+    /**
+     * Tarkistetaan osusiko arvaus. Mikäli osui, merkitään se vastustajan 
+     * karttaan ja palautetaan laiva johon osuttiin.
+     */
+    public static int[] tarkistaOsuma(String[][] vastustajanKartta, int[][] vastustajanLaivat, int rivi, int sarake){
+        if(vastustajanKartta[rivi][sarake].equals(Vakiot.merkit[1])){
+            // Jos osui merkitään se vastustajan karttaan.
+            System.out.println("\nRäjähdyssss! Osuit Laivaan!");
+            vastustajanKartta[rivi][sarake] = Vakiot.merkit[2];     
+            
+            int[] osuttuLaiva = new int[4];
+            outerLoop: // Haetaan osuttu laiva vastustajan kartasta.
+            for(int[] laiva : vastustajanLaivat){
+                // Oikea laiva jos laivan koordinaatit sama pelaajan arvaamien koordinattien kanssa.
+                for (int i = laiva[0]; i <= laiva[1]; i++) {
+                    for (int j = laiva[2]; j <= laiva[3]; j++) {
+                        if(i == rivi && j == sarake){
+                            osuttuLaiva = laiva;
+                            break outerLoop;
                         }
                     }
-                    else{
-                        System.out.println("\nOhi");
-                    }
-                    break;
+                }
             }
-        } catch (Exception e){
-            System.out.println("Yritä uudelleen...");
-        }   
+            return osuttuLaiva;
+        }
+        return new int[0];
+    }
+
+    /**
+     * Tarkistetaan upposiko laiva.
+     * Mikäli upposi: palautetaan true. Jos ei: false
+     */
+    public static boolean upposko(int[] osuttuLaiva, String[][] vastustajanKartta){
+        boolean upposko = false;
+        outerLoop: // Tarkistetaan upposiko laiva arvauksella.
+        for(int i = osuttuLaiva[0]; i <= osuttuLaiva[1]; i++){
+            for (int j = osuttuLaiva[2]; j <= osuttuLaiva[3]; j++){
+                // Jos osutun laivan jokaisessa osassa on osuma: laiva uppoaa.
+                if(vastustajanKartta[i][j].equals(Vakiot.merkit[2])){
+                    upposko = true;
+                } else{ // Jos mitä tahansa muuta: ei uppoa.
+                    upposko = false;
+                    break outerLoop;
+                }
+            }
+        }
+        return upposko;
     }
 
     /**
@@ -141,6 +169,7 @@ public class Peli {
         }
         if(voitto){ // Mikäli voittaja selvillä peli loppuu.
             System.out.println("\nPeli loppui.\n" + "Voittaja on: " + pelaaja);
+            voittaja = pelaaja;
             peliPaalla = false;
         }
     }
